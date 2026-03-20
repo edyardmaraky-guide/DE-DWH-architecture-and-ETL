f4971ee151bba26349ee9b432ceece52872fdbe0
Eduard Novikau <enovikau@ibagroup.eu>
2026-03-20 17:55:05 +0300

    /

--- sql/oltp_init.sql
+++ sql/oltp_init.sql
@@ -1,20 +1,22 @@
-CREATE TABLE customers (
+CREATE TABLE IF NOT EXISTS customers (
     customer_id SERIAL PRIMARY KEY,
     name VARCHAR(20),
     age INT,
     gender VARCHAR(5),
-    region VARCHAR(50)
+    region VARCHAR(50),
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE products (
+CREATE TABLE IF NOT EXISTS products (
     product_id SERIAL PRIMARY KEY,
     product_name VARCHAR(20),
     category VARCHAR(20),
     supplier_id VARCHAR(20),
-    cost_price DECIMAL(10,2)
+    cost_price DECIMAL(10,2),
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE sales (
+CREATE TABLE IF NOT EXISTS sales (
     sales_id INT PRIMARY KEY,
     customer_id INT,
     product_id INT,
